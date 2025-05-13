b4a3d86d14cab029e27d8cba0aac1864f179bbab
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2025-05-13 21:26:15 +0500

    finished preparations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/main.css'
+import '@/assets/styles/main.scss'
+import App from '@/App.vue'
 
 createApp(App).mount('#app')
